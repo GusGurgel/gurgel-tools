bf32c1134e3f075c29dac88c4a40db1fa35f445f
GusGurgel <gurgelg15@gmail.com>
2026-08-12 15:33:51 -0300

    FEAT: Adding lucide icons.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def index():
-    # Returns the HTML file directly to the browser
-    return FileResponse('apps/hello_world.html')
+    return FileResponse('apps/lucide.html')
 
 @app.get("/css/pico.min.css")
 async def css_pico():
-    # Returns the HTML file directly to the browser
     return FileResponse('pico/css/pico.min.css')
