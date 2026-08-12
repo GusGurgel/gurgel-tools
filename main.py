d16d170f61b6eb23a3fb0092060d0696c0bb88ee
GusGurgel <gurgelg15@gmail.com>
2026-08-12 15:13:57 -0300

    FEAT: Add first app example.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def index():
+    # Returns the HTML file directly to the browser
+    return FileResponse('apps/hello_world.html')
+
+@app.get("/css/pico.min.css")
+async def css_pico():
+    # Returns the HTML file directly to the browser
+    return FileResponse('pico/css/pico.min.css')
