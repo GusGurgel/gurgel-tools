39e9c2ed71112fed7bec26f4a5f265348f7fc5ae
GusGurgel <gurgelg15@gmail.com>
2026-08-12 16:29:51 -0300

    FEAT: Adjust folder structure.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
+main_path = Path(__file__).resolve().parents[1]
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
 async def index():
-    return FileResponse('apps/lucide.html')
+    return FileResponse(main_path / "src/apps/index/index.html")
+
 
 @app.get("/css/pico.min.css")
 async def css_pico():
-    return FileResponse('pico/css/pico.min.css')
+    return FileResponse("./pico/css/pico.min.css")
